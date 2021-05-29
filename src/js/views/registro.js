e70e05a42b7208bbb21b28f518ee3f4b1b0561ad
marcosspe <marcos.perez.garcia0@gmail.com>
2021-05-29 16:45:51 +0000

    creada vistas entrar y registro

--- src/js/views/registro.js
+++ src/js/views/registro.js
@@ -1,19 +1,50 @@
 import React, { useState, useEffect, useContext } from "react";
 
 export const Registro = () => (
-    <div className="container">
-        <div className="">
-            <div className="col-4">
-                <p className="card-text">First_name:</p>
-                <input
-                    onChange={actions.signupData}
-                    name="first_name"
-                    type="text"
-                    className="form-control"
-                    placeholder="first_name"
-                    aria-label="first_name"
-                />
-            </div>
-        </div>
-    </div>
+	<div className="container mt-5">
+		<div className="container mt-5">
+			<h3 className="card-title text-center text-warning mt-5 mb-5">Registro</h3>
+			<div className="row">
+				<div className="col">
+					<p className="card-text text-warning">First_name:</p>
+					<input
+						name="first_name"
+						type="text"
+						className="form-control"
+						placeholder="first_name"
+						aria-label="first_name"
+					/>
+				</div>
+				<div className="col">
+					<p className="card-text text-warning">Last_name:</p>
+					<input
+						name="last_name"
+						type="text"
+						className="form-control"
+						placeholder="last_name"
+						aria-label="last_name"
+					/>
+				</div>
+			</div>
+			<div className="row">
+				<div className="col">
+					<p className="card-text text-warning">Email</p>
+					<input name="email" type="text" className="form-control" placeholder="email" aria-label="email" />
+				</div>
+				<div className="col">
+					<p className="card-text text-warning">Password</p>
+					<input
+						name="password"
+						type="password"
+						className="form-control"
+						placeholder="********"
+						aria-label="********"
+					/>
+				</div>
+			</div>
+			<div className="text-right mt-2">
+				<button className="btn btn-outline-warning mr-2">Registro</button>
+			</div>
+		</div>
+	</div>
 );
